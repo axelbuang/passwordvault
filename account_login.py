a015f517fb4274595a096f24e664f446a1297b15
Jhon Axel Marañon <45385617+axelbuang@users.noreply.github.com>
2019-07-09 16:49:32 +0800

    deleted some useless variables; defining login

--- account_login.py
+++ account_login.py
@@ -1,13 +1,7 @@
-name = input('NAME:')
-website = input('WEBSITE:  ')
-email = input('EMAIL:    ')
-username = input('USERNAME: ')
-password = input('PASSWORD: ')
-notes = input('NOTES: ')
-if password is None:
-    print('No PASSWORD')
-    print(e)
-
-done = True
-if done:
-    print('done')
+def login()
+    name = input('NAME:')
+    website = input('WEBSITE:  ')
+    email = input('EMAIL:    ')
+    username = input('USERNAME: ')
+    password = input('PASSWORD: ')
+    
