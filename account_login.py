d429bbe357c87f449503de5572079e9aeecd870c
Jhon Axel Marañon <45385617+axelbuang@users.noreply.github.com>
2019-07-09 17:18:12 +0800

    filename renamed; imported some package

--- account_login.py
+++
@@ -1,6 +0,0 @@
-def login():
-    name = input('Name of Organization: ')
-    website = input('Website/Domain:')
-    email = input('Email: ')
-    username = input('Username: ')
-    password = input('Password: ')
---
+++ data_login.py
@@ -0,0 +1,8 @@
+def login():
+    from hashlib_test import username, pass_hasher
+    name = input('Name of Organization: ')
+    website = input('Website/Domain:')
+    email = input('Email: ')
+    username(input('Username: '))
+    pass_hasher(input('Password: '))
+    
