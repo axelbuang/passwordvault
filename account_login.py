d3a3396fea0e8a3c727a67550967e0f3ef9f690d
Jhon Axel Marañon <45385617+axelbuang@users.noreply.github.com>
2019-07-09 16:51:03 +0800

    Update account_login.py

--- account_login.py
+++ account_login.py
@@ -1,7 +1,6 @@
 def login()
-    name = input('NAME:')
-    website = input('WEBSITE:  ')
-    email = input('EMAIL:    ')
-    username = input('USERNAME: ')
-    password = input('PASSWORD: ')
-    
+    name = input('Name of Organization: ')
+    website = input('Website/Domain:')
+    email = input('Email: ')
+    username = input('Username: ')
+    password = input('Password: ')
