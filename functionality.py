2464949c72ff962df604124047f3755e3c848df7
Jhon Axel Marañon <45385617+axelbuang@users.noreply.github.com>
2019-07-13 19:59:53 +0800

    datetime + while loop was added

--- functionality.py
+++ functionality.py
@@ -1,16 +1,39 @@
 # This is for the functionality of the application when run in terminal.
 
 import sys
-import datetime
+from datetime import datetime as dt
+from data_login import Information
 
-opening = "Password Vault 1.0 \n'Put me some Day of the week, DD/MM/YYYY, and timezone' \n"
-what_to_do = '''SYNTAX 
-    pv -add : Add new data.
-    pv -remv: Remove existing data.
-    
+title = "Password Vault 1.0"
+day_of_the_year  =   dt.now().strftime("%j")
+date_today = dt.now().strftime("%A, %d %B %Y")
+time_today = dt.now().strftime("%H : %M : %S")
+                                    
+what_to_do = '''SYNTAX
+        -a, -add : Add new data.
+        -r, -rm : Remove existing data.      
 MISC
-    pv -help : Go to help.
-    pv -x : Exit.'''
-
-print(opening)
+        -V : Print version.
+        -help : Go to help.
+        -x : Exit.
+'''
+print(title)
+print('Day ' +  day_of_the_year + ":  " + date_today  + ", "  + time_today )
 print(what_to_do)
+while True:
+
+    i = input('vault@your_stupid_computer~$ ')
+        
+    if i == "-a" or i == "-add":
+#         Information.add_data(info)
+        pass
+    elif i == "-rm":
+        pass      
+    elif i == '-x':
+        exit()
+    elif i == '':
+        continue
+    else:
+        str_i = str(i)
+        print("[UNKNOWN]: " + "'" + str_i + "'" + " is not a command.")
+        continue
