0fc80a08fe28ec0f536a61c18035addc0bd72471
Jhon Axel Marañon <45385617+axelbuang@users.noreply.github.com>
2019-07-18 21:51:28 +0800

    Deleted passvault_art

--- functionality.py
+++ functionality.py
@@ -1,37 +1,37 @@
 # This is for the functionality of the application when run in terminal.
 
-import sys
+import data_login
 from datetime import datetime as dt
-from data_login import Information
-import passvault_art as p_art
 
 title = "Password Vault 1.0"
+
+# will print the weekday, date, month, and year respectively
 day_of_the_year = dt.now().strftime("%j")
 date_today = dt.now().strftime("%A, %d %B %Y")
 time_today = dt.now().strftime("%H : %M : %S")
-                                    
-what_to_do = '''SYNTAX
+
+what_to_do = '''[USAGE]
         -a, -add : Add new data.
-        -r, -rm : Remove existing data.      
-MISC
+             -rm : Remove existing data.
+[MISC]
         -V : Print version.
         -help : Go to help.
         -x : Exit.
 '''
 print(title)
-p_art.art()
 print('Day ' +  day_of_the_year + ":  " + date_today  + ", "  + time_today )
 print(what_to_do)
 
 while True:
 
     i = input('vault@your_stupid_computer~$ ')
-        
+
     if i == "-a" or i == "-add":
-#         Information.add_data(info)
-        pass
+        import data_login
+        data_login.main()
     elif i == "-rm":
-        pass      
+        # pass
+        pass
     elif i == '-x':
         exit()
     elif i == '':
@@ -40,3 +40,4 @@ while True:
         str_i = str(i)
         print("[UNKNOWN]: " + "'" + str_i + "'" + " is not a command.")
         continue
+
